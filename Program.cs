d15c76595789cbf7b6ad62e42ca74ab93dd31838
劉敦桐 <zse811022@hotmail.com>
2023-09-03 17:09:19 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string Name=Console.ReadLine();
+			//Console.WriteLine("Hello world C#");
+			Console.WriteLine($"Hello world {Name}");
+		}
+	}
+}
